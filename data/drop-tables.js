15f70a97a1d8495d55cdb76aaa8ecb3faebd608d
harriet-the-spy <dpcairns@gmail.com>
2021-03-02 14:05:29 -0800

    add

--- data/drop-tables.js
+++ data/drop-tables.js
@@ -9,8 +9,8 @@ async function run() {
     await client.connect();
     
     await client.query(`
-            DROP TABLE IF EXISTS users CASCADE;
-            DROP TABLE IF EXISTS animals;
+      DROP TABLE IF EXISTS users CASCADE;
+      DROP TABLE IF EXISTS todos;
         `);
 
     console.log(' drop tables complete', getEmoji(), getEmoji(), getEmoji());
---
+++ data/todos.js
@@ -0,0 +1,14 @@
+module.exports = [
+  {
+    todo: 'do dishes',
+    completed: false,
+  },
+  {
+    todo: 'mow lawn',
+    completed: false,
+  },
+  {
+    todo: 'scrub a floor',
+    completed: false,
+  }
+];
